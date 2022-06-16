74a69f2d3c97f754e1ae747ef6f6e1f9236e2ac9
Kaijlo <kaijserlove@gmail.com>
2022-06-16 09:00:07 +0200

    wasm completed

--- wasmTest/exec.go
+++ wasmTest/exec.go
@@ -2,21 +2,49 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
-	"reflect"
-	"unsafe"
+	"strconv"
+	"time"
 
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/metrics"
+	"github.com/open-policy-agent/opa/topdown"
+	"github.com/open-policy-agent/opa/topdown/builtins"
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
 	"github.com/tetratelabs/wazero/wasi_snapshot_preview1"
 )
 
+var (
+	env, opa   api.Module
+	ctx        context.Context
+	builtinMap map[int32]topdown.BuiltinFunc
+	tCTX       *topdown.BuiltinContext
+)
+
 func main() {
 
 	// Choose the context to use for function calls.
-	ctx := context.Background()
+	ctx = context.Background()
+	tCTX = &topdown.BuiltinContext{
+		Context:      ctx,
+		Metrics:      metrics.New(),
+		Seed:         rand.New(rand.NewSource(0)),
+		Time:         ast.NumberTerm(json.Number(strconv.FormatInt(time.Now().UnixNano(), 10))),
+		Cancel:       topdown.NewCancel(),
+		Runtime:      nil,
+		Cache:        make(builtins.Cache),
+		Location:     nil,
+		Tracers:      nil,
+		QueryTracers: nil,
+		QueryID:      0,
+		ParentID:     0,
+	}
 	// Read a WebAssembly binary containing an exported "fac" function.
 	// * Ex. (func (export "fac") (param i64) (result i64) ...
 	wasm, err := os.ReadFile("./policy.wasm")
@@ -30,78 +58,90 @@ func main() {
 	if _, err = wasi_snapshot_preview1.Instantiate(ctx, r); err != nil {
 		log.Panicln(err)
 	}
-	env, err := r.NewModuleBuilder("env").
+	env, err = r.NewModuleBuilder("env").
 		ExportFunction("opa_abort", opaAbort).
-		ExportFunction("opa_builtin0", func(i, j int32) int32 { return i }).
-		ExportFunction("opa_builtin1", func(i, j, k int32) int32 { return i }).
-		ExportFunction("opa_builtin2", func(i, j, k, l int32) int32 { return i }).
-		ExportFunction("opa_builtin3", func(i, j, k, l, m int32) int32 { return i }).
-		ExportFunction("opa_builtin4", func(i, j, k, l, m, n int32) int32 { return i }).
+		ExportFunction("opa_builtin0", C0).
+		ExportFunction("opa_builtin1", C1).
+		ExportFunction("opa_builtin2", C2).
+		ExportFunction("opa_builtin3", C3).
+		ExportFunction("opa_builtin4", C4).
 		ExportFunction("opa_println", opaPrintln).
-		ExportMemory("memory", 2).
+		ExportMemory("memory", 10).
 		Instantiate(ctx, r)
 	if err != nil {
 		log.Panicln(err)
 	}
-	module, err := r.InstantiateModuleFromBinary(ctx, wasm)
+	opa, err = r.InstantiateModuleFromBinary(ctx, wasm)
+	if err != nil {
+		log.Panicln(err)
+	}
+	builtins, err := opa.ExportedFunction("builtins").Call(ctx)
+	if err != nil {
+		log.Panicln(err)
+	}
+	builtinsJson, err := opa.ExportedFunction("opa_json_dump").Call(ctx, builtins[0])
+	if err != nil {
+		log.Panicln(err)
+	}
+	builtinMap, err = getFuncs(parseJsonBuiltinString(readStr(uint32(builtinsJson[0]), ctx)))
 	if err != nil {
 		log.Panicln(err)
 	}
 	//env.Name()
 	// Discover 7! is 5040
-	ev, err := module.ExportedFunction("opa_eval_ctx_new").Call(ctx)
+	ev, err := opa.ExportedFunction("opa_eval_ctx_new").Call(ctx)
 	if err != nil {
 		log.Panicln(err)
 	}
-	input := []byte("{\"user\":\"alice\",\"method\":\"post\"}")
-	loc, err := module.ExportedFunction("opa_malloc").Call(ctx, uint64(len(input)))
+	input := []byte("{\"user\":\"alice\",\"method\":\"get\"}")
+	loc, err := opa.ExportedFunction("opa_malloc").Call(ctx, uint64(len(input)))
 	if err != nil {
 		log.Panicln(err)
 	}
 	env.Memory().Write(ctx, uint32(loc[0]), input)
-	inp, err := module.ExportedFunction("opa_json_parse").Call(ctx, loc[0], uint64(len(input)))
+	inp, err := opa.ExportedFunction("opa_json_parse").Call(ctx, loc[0], uint64(len(input)))
 	if err != nil {
 		log.Panicln(err)
 	}
-	_, err = module.ExportedFunction("opa_eval_ctx_set_input").Call(ctx, ev[0], inp[0])
+	_, err = opa.ExportedFunction("opa_eval_ctx_set_input").Call(ctx, ev[0], inp[0])
 	if err != nil {
 		log.Panicln(err)
 	}
 	data := []byte("{\"dataset\":{\"methods\":[\"get\",\"post\",\"put\",\"delete\"],\"users\":{\"alice\":[\"get\"],\"bob\":[\"get\",\"post\"],\"charlie\":[\"get\",\"post\",\"put\"],\"dana\":[\"get\",\"post\",\"put\",\"delete\"]}}}")
-	dLoc, err := module.ExportedFunction("opa_malloc").Call(ctx, uint64(len(data)))
+	dLoc, err := opa.ExportedFunction("opa_malloc").Call(ctx, uint64(len(data)))
 	if err != nil {
 		log.Panicln(err)
 	}
 	env.Memory().Write(ctx, uint32(dLoc[0]), data)
-	dat, err := module.ExportedFunction("opa_json_parse").Call(ctx, dLoc[0], uint64(len(data)))
+	dat, err := opa.ExportedFunction("opa_json_parse").Call(ctx, dLoc[0], uint64(len(data)))
 	if err != nil {
 		log.Panicln(err)
 	}
-	_, err = module.ExportedFunction("opa_eval_ctx_set_data").Call(ctx, ev[0], dat[0])
+	_, err = opa.ExportedFunction("opa_eval_ctx_set_data").Call(ctx, ev[0], dat[0])
 	if err != nil {
 		log.Panicln(err)
 	}
-	_, err = module.ExportedFunction("eval").Call(ctx, ev[0])
+	_, err = opa.ExportedFunction("eval").Call(ctx, ev[0])
 	if err != nil {
 		log.Panicln(err)
 	}
-	resLoc, err := module.ExportedFunction("opa_eval_ctx_get_result").Call(ctx, ev[0])
+	resLoc, err := opa.ExportedFunction("opa_eval_ctx_get_result").Call(ctx, ev[0])
 	if err != nil {
 		log.Panicln(err)
 	}
-	strLoc, err := module.ExportedFunction("opa_json_dump").Call(ctx, resLoc[0])
+	strLoc, err := opa.ExportedFunction("opa_json_dump").Call(ctx, resLoc[0])
 	if err != nil {
 		log.Panicln(err)
 	}
-	fmt.Println(readStr(env.Memory(), uint32(strLoc[0]), ctx))
-	env.ExportedFunction("opa_println").Call(ctx, strLoc[0])
+	fmt.Println(readStr(uint32(strLoc[0]), ctx))
+	env.ExportedFunction("opa_abort").Call(ctx, strLoc[0])
 	fmt.Println(context.Background(), ctx)
 }
-func readStr(mem api.Memory, loc uint32, ctx context.Context) string {
+func readStr(loc uint32, ctx context.Context) string {
 	bytes := []byte{}
 	var index uint32 = 0
 	for ok := true; ok; {
-		b, _ := mem.ReadByte(ctx, loc+index)
+		b, _ := env.Memory().ReadByte(ctx, loc+index)
 		if b == 0b0 {
 			ok = false
 		} else {
@@ -115,11 +155,149 @@ func readStr(mem api.Memory, loc uint32, ctx context.Context) string {
 	}
 	return out
 }
-func opaAbort(i int32) {
-	panic(string(i))
+func opaAbort(ptr int32) {
+	bytes := []byte{}
+	var index uint32 = 0
+	for ok := true; ok; {
+		b, _ := env.Memory().ReadByte(context.Background(), uint32(ptr)+index)
+		if b == 0b0 {
+			ok = false
+		} else {
+			bytes = append(bytes, b)
+		}
+		index++
+	}
+	out := ""
+	for _, b := range bytes {
+		out += string(b)
+	}
+	log.Panic("error", out)
+}
+func Call(args ...int32) int32 {
+	var output *ast.Term
+	pArgs := []*ast.Term{}
+	for _, ter := range args[2:] {
+		serialized, err := opa.ExportedFunction("opa_value_dump").Call(ctx, uint64(ter))
+		if err != nil {
+			log.Panic(err)
+		}
+		data := readStr(uint32(serialized[0]), ctx)
+		pTer, err := ast.ParseTerm(string(data))
+		if err != nil {
+			log.Panic(err)
+		}
+		pArgs = append(pArgs, pTer)
+	}
+	err := builtinMap[args[0]](*tCTX, pArgs, func(t *ast.Term) error {
+		output = t
+		return nil
+	})
+	if err != nil {
+		if errors.As(err, &topdown.Halt{}) {
+			var e *topdown.Error
+			if errors.As(err, &e) && e.Code == topdown.CancelErr {
+				log.Panic(err)
+			}
+			log.Panic(err)
+		}
+		// non-halt errors are treated as undefined ("non-strict eval" is the only
+		// mode in wasm), the `output == nil` case below will return NULL
+	}
+	if output == nil {
+		return 0
+	}
+	outB := []byte(output.String())
+	loc, err := opa.ExportedFunction("opa_malloc").Call(ctx, uint64(len(outB)))
+	if err != nil {
+		log.Panic(err)
+	}
+	env.Memory().Write(ctx, uint32(loc[0]), outB)
+	addr, err := opa.ExportedFunction("opa_value_parse").Call(ctx, uint64(loc[0]), uint64(len(outB)))
+	if err != nil {
+		log.Panic(err)
+	}
+	return int32(addr[0])
+}
+func C0(i, j int32) int32 {
+	fmt.Println(builtinMap[i])
+	return Call(i, j)
+}
+func C1(i, j, k int32) int32 {
+	fmt.Println(builtinMap[i])
+	return Call(i, j, k)
+}
+func C2(i, j, k, l int32) int32 {
+	fmt.Println(builtinMap[i])
+	return Call(i, j, k, l)
+}
+func C3(i, j, k, l, m int32) int32 {
+	fmt.Println(builtinMap[i])
+	return Call(i, j, k, l, m)
+}
+func C4(i, j, k, l, m, n int32) int32 {
+	fmt.Println(builtinMap[i])
+	return Call(i, j, k, l, m, n)
 }
 func opaPrintln(ptr int32) {
 
-	fmt.Println(*(*string)(unsafe.Pointer(&reflect.SliceHeader{Data: uintptr(ptr), Len: 0, Cap: 0})))
+	bytes := []byte{}
+	var index uint32 = 0
+	for ok := true; ok; {
+		b, _ := env.Memory().ReadByte(context.Background(), uint32(ptr)+index)
+		if b == 0b0 {
+			ok = false
+		} else {
+			bytes = append(bytes, b)
+		}
+		index++
+	}
+	out := ""
+	for _, b := range bytes {
+		out += string(b)
+	}
+	fmt.Println(out)
 
+}
+func getFuncs(ids map[string]int32) (map[int32]topdown.BuiltinFunc, error) {
+	out := map[int32]topdown.BuiltinFunc{}
+	for name, id := range ids {
+		out[id] = topdown.GetBuiltin(name)
+		if out[id] == nil {
+			return out, fmt.Errorf("no function named %s", name)
+		}
+	}
+	return out, nil
+}
+func parseJsonBuiltinString(str string) map[string]int32 {
+	currKey := ""
+	inKey := false
+	inVal := false
+	currVal := ""
+	out := map[string]int32{}
+	for _, char := range str {
+		switch char {
+		case '"':
+			inKey = !inKey
+		case '{':
+		case '}':
+			val, _ := strconv.ParseInt(currVal, 10, 32)
+			out[currKey] = int32(val)
+		case ':':
+			inVal = true
+		case ',':
+			val, _ := strconv.ParseInt(currVal, 10, 32)
+			out[currKey] = int32(val)
+			inVal = false
+			currVal = ""
+			currKey = ""
+		default:
+			if inKey {
+				currKey += string(char)
+			} else if inVal {
+				currVal += string(char)
+			}
+		}
+
+	}
+	return out
 }
--- wasmTest/regoPolicies/test.rego
+++ wasmTest/regoPolicies/test.rego
@@ -2,6 +2,11 @@ package rules
 default valid_method=false
 default valid_user=false
 default allow=false
+httpReq:=http.send({"url":"https://httpbin.org/get","method":"GET","raise_error":false})
+jsonSmashed:=json.patch({"a": {"foo": 1}}, [{"op": "add", "path": "/a/bar", "value": 2}])
+tester (x)=y{
+  y:=abs(x)
+}
 valid_method {
   some i
     data.dataset.methods[i]==input.method
@@ -31,4 +36,6 @@ message[reason] {
   allow
   reason := {"message":"Successfully executed the requested method"}
 }
-response:={"allow":allow,"message":message}
+
+response:={"allow":allow,"message":message,"test":tester(-1),"http":httpReq,"json":jsonSmashed}
+
